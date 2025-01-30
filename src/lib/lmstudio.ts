1e2740b1538604f954d3a104999a6119a8239f69
ikumasudo <44293565+ikumasudo@users.noreply.github.com>
2025-01-30 22:04:57 +0900

    変更

--- src/lib/lmstudio.ts
+++ src/lib/lmstudio.ts
@@ -2,7 +2,7 @@ import { createOpenAICompatible } from "@ai-sdk/openai-compatible";
 
 const lmstudio = createOpenAICompatible({
   name: "lmstudio",
-  baseURL: "http://desktop-win:1234/v1",
+  baseURL: "https://desktop-win.tailb587a6.ts.net/v1",
 });
 
 const model = lmstudio("lmstudio-community/Qwen2.5-7B-Instruct-1M-GGUF");
